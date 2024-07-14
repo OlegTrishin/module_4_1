9754535858e209f7bb13b505238fb6d6fc8eb88a
Oleg <otrishin@gmail.com>
2024-07-14 20:28:04 +1000

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+# fake_math.py
+def fake_divide(first, second):
+    if second != 0:
+        return first / second
+    else:
+        return "Ошибка"
+    
---
+++ true_math.py
@@ -0,0 +1,8 @@
+# true_math.py
+from math import inf
+
+def true_divide(first, second):
+    if second != 0:
+        return first / second
+    else:
+        return inf
